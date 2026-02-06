28f972a2e5ecc95a678073c5414146244527ee61
阿泽 <m15265789032@163.com>
2026-02-06 15:53:36 +0800

    Update and rename natfrp.sh to natfrp_install.sh

--- natfrp_install.sh
+++ natfrp_install.sh
@@ -2,7 +2,6 @@ wget https://nya.globalslb.net/natfrp/client/launcher-unix/3.1.7/natfrp-service_
 tar -I zstd -xvf natfrp-service_linux_amd64.tar.zst
 sudo chmod +x frpc
 sudo ./frpc
-rm README.en.md
 rm README.md
 rm natfrp-service_linux_amd64.tar.zst
 rm ../natfrp_install.sh
