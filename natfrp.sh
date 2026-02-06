a132ab5f8c7cb04faa627828ac685e5825961399
阿泽 <m15265789032@163.com>
2026-02-06 15:49:54 +0800

    Install and run natfrp client
    Download and execute natfrp client, clean up files.

---
+++ natfrp.sh
@@ -0,0 +1,8 @@
+wget https://nya.globalslb.net/natfrp/client/launcher-unix/3.1.7/natfrp-service_linux_amd64.tar.zst
+tar -I zstd -xvf natfrp-service_linux_amd64.tar.zst
+sudo chmod +x frpc
+sudo ./frpc
+rm README.en.md
+rm README.md
+rm natfrp-service_linux_amd64.tar.zst
+rm ../natfrp_install.sh
